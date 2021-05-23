88a1265259b04acff73102facd362792eacc7819
Janssen Batista <batistajanssen.dev@gmail.com>
2021-05-23 14:30:44 -0300

    Removed unused package utils

--- app/src/main/java/br/com/jdev/simplecurrencyconverter/utils/Constant.kt
+++
@@ -1,7 +0,0 @@
-package br.com.jdev.simplecurrencyconverter.utils
-
-object Constant {
-    const val BASE_URL = "https://free.currconv.com"
-    // Replace with your api key obtained in https://www.currencyconverterapi.com/
-    const val API_KEY = "YOUR_API_KEY"
-}
